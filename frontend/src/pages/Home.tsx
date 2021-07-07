90867bc4965fef7a40cd6a086703d6bbccc4f7de
Pedro Thiago <ptmarmello@gmail.com>
2021-07-07 03:12:03 -0300

    mais uma modificação rápida

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { auth, firebase } from '../services/firebase';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoTmg from '../assets/images/logo.svg'
@@ -8,15 +8,14 @@ import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
 export function Home() {
-    // const history = useHistory();
+    const history = useHistory();
 
     function handleCreateRoom(){
         const provider = new firebase.auth.GoogleAuthProvider();
         auth.signInWithPopup(provider).then(result =>{
-            console.log(result);
+            history.push('/rooms/new');
         })
 
-        // history.push('/rooms/new');
     }
 
     return(
